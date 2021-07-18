4afdf21190b100e3b868decc2577298f4e5040d9
root <dloewen481@gmail.com>
2021-07-18 20:22:49 +0000

    Added barebones

---
+++ db.rb
@@ -0,0 +1,39 @@
+# David "Mesp" Loewen
+# DB accessor file
+
+require "sqlite3"
+
+def opendb
+	return SQLite3::Database.new "draft_league.db"
+
+# DELETES THE DATABASE, then recreates it
+def setup
+	db = opendb
+	db.execute <<-SQL
+		CREATE TABLE IF NOT EXISTS players(
+			id varchar(30) NOT NULL PRIMARY KEY
+		);
+	SQL
+	db.execute <<-SQL
+		CREATE TABLE IF NOT EXISTS seasons(
+			id int NOT NULL PRIMARY KEY,
+			starttime date,
+			endtime date
+		);
+	SQL
+	db.execute <<-SQL
+		CREATE TABLE IF NOT EXISTS player_records(
+			playerid varchar(30) NOT NULL,
+			seasonid int NOT NULL,
+			wins int,
+			losses int,
+			placement int,
+			CONSTRAINT pk PRIMARY KEY (playerid, seasonid),
+			FOREIGN KEY (playerid) REFERENCES players(id) ON DELETE CASCADE,
+			FOREIGN KEY (seasonid) REFERENCES seasons(id) ON DELETE CASCADE
+		);
+	SQL
+	# TODO
+	db.execute <<-SQL
+		CREATE TABLE IF NOT EXISTS pokemon();
+	SQL
