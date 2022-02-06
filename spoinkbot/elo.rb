942228ab31bd3597379656ae56ac14e4d20493f0
root <dloewen481@gmail.com>
2022-02-06 21:09:29 +0000

    up to s5w4, diff dict

--- spoinkbot/elo.rb
+++ spoinkbot/elo.rb
@@ -2,6 +2,7 @@ require 'csv'
 
 def reset_elo
     @elo_dict = Hash.new(1000)
+    @diff_dict = Hash.new(0)
 end
 
 def calc_match_probability(winner, loser)
@@ -17,10 +18,14 @@ def process_match(player1, player2, constant, outcome)
         # Player 1 wins
         @elo_dict[player1] = @elo_dict[player1] + constant * (1 - prob1)
         @elo_dict[player2] = @elo_dict[player2] + constant * (0 - prob2)
+        @diff_dict[player1] = constant * (1 - prob1)
+        @diff_dict[player2] = constant * (0 - prob2)
     else
         # Player 2 wins
         @elo_dict[player1] = @elo_dict[player1] + constant * (0 - prob1)
         @elo_dict[player2] = @elo_dict[player2] + constant * (1 - prob2)
+        @diff_dict[player1] = constant * (0 - prob1)
+        @diff_dict[player2] = constant * (1 - prob2)
     end
 
 end
@@ -34,7 +39,7 @@ def calc_elo_dict(player = nil)
     else
         sorted = @elo_dict.sort_by {|k, v| -v}
         sorted.each do |key,value|
-            print("#{key}: #{value.to_i}\n")
+            print("#{key}: #{value.to_i}\t(+#{@diff_dict[key].round(1)})\n".sub("+-","-"))
         end
         
     end
