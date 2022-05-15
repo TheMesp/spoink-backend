333e22712323d3598f6e066a8b540a1ca3fb9f0f
root <dloewen481@gmail.com>
2022-05-15 06:59:23 +0000

    Improved player get req logic

--- routes/players.js
+++ routes/players.js
@@ -37,20 +37,17 @@ router.get('/', function(req, res, next) {
 
 /* GET individual player. */
 router.get('/:id', loadPlayer, function(req, res, next) {
-  console.log("Got a request for players")
-  var sql = `SELECT * FROM players`
-  var params = []
-  db.all(sql, params,(err, rows) => {
-    if(err){
-      res.status(500).json({"error":err.message});
-      return;
-    }
-  console.log(rows);
-  res.json({
-    "message":"success",
-    "data":req.player
-  })
-  });
+  console.log("Got a request for player")
+  if(req.player){
+    res.json({
+      "message":"success",
+      "data":req.player
+    })
+    
+    return;
+  }else{
+    res.status(404).json({"error":`Player with id ${req.params.id} not found.`});
+  }
 });
 
 module.exports = router;
